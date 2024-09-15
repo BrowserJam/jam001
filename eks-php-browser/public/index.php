1b71e494c9c428182307e45f9c84459ab1259dda
Ernestas Kluonis <ekgame1@gmail.com>
2024-09-15 13:14:55 +0300

    Added browser "chrome"

--- eks-php-browser/public/index.php
+++ eks-php-browser/public/index.php
@@ -7,36 +7,66 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Klein\Klein;
 use Klein\Request;
 use Klein\Response;
+use League\Uri\Uri;
+use League\Uri\UriResolver;
 
 $klein = new Klein();
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader, [
-    'cache' => __DIR__ . '/../twig_cache',
+    'cache' => false,//__DIR__ . '/../twig_cache',
 ]);
 
-$klein->respond('GET', '/', function (Request $request, Response $response) use ($twig) {
+$default_link = 'https://info.cern.ch/hypertext/WWW/TheProject.html';
+
+$klein->respond('GET', '/', function (Request $request, Response $response) use ($twig, $default_link) {
     $request_link = $request->paramsGet()->get('link', null);
+    $page_width = (int)$request->paramsGet()->get('width', 800);
+    if ($page_width === null) {
+        $page_width = 800;
+    }
+
+    if ($page_width < 100 || $page_width > 2000) {
+        $page_width = 800;
+    }
+
+    $createBrowserLink = function (string $url) use ($page_width) {
+        $link = '/?width='.$page_width;
+        $link .= '&link='.urlencode($url);
+        return $link;
+    };
+    
     if ($request_link === null) {
-        $response->body('Missing link to render');
+        $response->redirect($createBrowserLink($default_link));
         return;
     }
 
-    $test_html = file_get_contents(__DIR__ . '/assets/test_main.html');
+    if (!str_starts_with($request_link, 'https://info.cern.ch/')) {
+        $response->redirect($createBrowserLink($default_link));
+        return;
+    }
+
+    // $html = file_get_contents(__DIR__ . '/assets/test_main.html');
+    $html = file_get_contents($request_link);
     $renderer = new \Ekgame\PhpBrowser\HtmlToImageRenderer();
-    $image = $renderer->render($test_html, 930);
+    $image = $renderer->render($html, $page_width);
     $areas = $renderer->getAreas();
 
+    $base_uri = Uri::createFromString($request_link);
     return $twig->render('index.html.twig', [
+        'current_link' => $base_uri->toString(),
+        'current_width' => $page_width,
         'image' => $image->toPng()->toDataUri(),
-        'areas' => array_map(function ($item) {
+        'areas' => array_map(function ($item) use ($base_uri, $createBrowserLink) {
             $raw_link = $item->getContext()['href'];
+            $relative_uri = Uri::createFromString($raw_link);
+            $absolute_uri = UriResolver::resolve($relative_uri, $base_uri);
             return [
                 'x1' => $item->getArea()->getX(),
                 'y1' => $item->getArea()->getY(),
                 'x2' => $item->getArea()->getX() + $item->getArea()->getWidth(),
                 'y2' => $item->getArea()->getY() + $item->getArea()->getHeight(),
-                'href' => $raw_link,
-                'alt' => $raw_link,
+                'href' => $createBrowserLink($absolute_uri->toString()),
+                'alt' => $absolute_uri->toString(),
             ];
         }, $areas)
     ]);
